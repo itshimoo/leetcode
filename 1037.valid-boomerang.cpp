07e7978e4cc8a674887c7ed0beffc92bfbf2d928
Ibrahim Helal <ihelal@misorobotics.com>
2023-12-24 19:07:31 -0800

    Valid Boomerang

---
+++ 1037.valid-boomerang.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1037 lang=cpp
+ *
+ * [1037] Valid Boomerang
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        
+    }
+};
+// @lc code=end
+
